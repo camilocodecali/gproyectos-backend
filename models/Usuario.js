2e1cd4504ad38d40cc85e8e48da9277d45844961
Camilo Rodriguez <camilorodriguezco7@gmail.com>
2023-07-20 17:16:36 -0500

    Crearmos funcion para registrar, autenticar y confirmar un usuario. Creamos Helpers para Generar Tokens y Hashear contraseña, backend funcionando con modelos controladores y rutas

--- models/Usuario.js
+++ models/Usuario.js
@@ -1,54 +1,69 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-const usuarioSchema = mongoose.Schema({
+const usuarioSchema = mongoose.Schema(
+  {
     nombre: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     password: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     email: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true
+      type: String,
+      required: true,
+      trim: true,
+      unique: true,
     },
     telefono: {
-        type: String,
-        required: true,
-        trim: true,
+      type: String,
+      required: true,
+      trim: true,
     },
     cargo: {
-        type: String,
-        required: true,
-        enum: ["Lider", "Colaborador", "Admin"]
+      type: String,
+      required: true,
+      enum: ["Lider", "Colaborador", "Admin"],
     },
     fechaIngreso: {
-        type: Date,
-        required: true,
-        default: Date.now()
+      type: Date,
+      required: true,
+      default: Date.now(),
     },
     identificacion: {
-        type: String,
-        required: true,
-        trim: true
+      type: String,
+      required: true,
+      trim: true,
     },
     token: {
-        type: String,
+      type: String,
     },
     confirmado: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
-},
-    {
-        timestamps: true,
-    }
+  },
+  {
+    timestamps: true,
+  }
 );
+
+usuarioSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+usuarioSchema.methods.comprobarPassword = async function (passwordFormulario){
+    return await bcrypt.compare(passwordFormulario, this.password)
+}
 
 const Usuario = mongoose.model("Usuario", usuarioSchema);
 
---
+++ routes/usuarioRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+const router = express.Router();
+import { registrar, autenticar, confirmar } from "../controllers/usuarioController.js";
+
+//Autenticación, Registro y Confirmación de Usuarios
+router.post('/', registrar)
+router.post('/login', autenticar)
+router.get('/confirmar/:token', confirmar)
+
+
+export default router
