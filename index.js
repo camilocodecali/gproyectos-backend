6f5c505ab19568da441d2545c499dba5c73feea3
Camilo Rodriguez <camilorodriguezco7@gmail.com>
2023-07-20 01:58:29 -0500

    Primer Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import  express  from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/db.js";
+
+
+const app = express();
+
+dotenv.config();
+
+conectarDB();
+
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, ()=> {
+    console.log("Servidor Corriendo desde el puerto 4000");
+})
+
